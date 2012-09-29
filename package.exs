cb70d0bbff90cb6d5e610b3b5c5aec1bca84c3d7
Yurii Rashkovskii <yrashk@gmail.com>
2012-09-29 07:36:59 -0700

    Add expm package.exs

---
+++ package.exs
@@ -0,0 +1,5 @@
+Expm.Package.new(name: "properex", description: "PropEr support for Elixir",
+                 version: "0.1", keywords: ["test","random","property"], 
+                 maintainers: [[name: "Yurii Rashkovskii", email: "yrashk@gmail.com"]],
+                 contributors: [[name: "ILYA Khloptov", email: "ilya@spawngrid.com"]],
+                 repositories: [[github: "yrashk/properex", tag: "0.1"]])
